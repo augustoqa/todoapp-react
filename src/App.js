43e9ee79082617870a248214bb25bee7d4d15d74
Cesar Acual <chechaacual@gmail.com>
2020-03-28 16:09:27 -0600

    Moving State Up - Adding state to Counter Component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Counter by={1}/>
-      <Counter by={5}/>
-      <Counter by={10}/>
+      <Counter />
     </div>
   );
 }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -17,10 +17,40 @@ class Counter extends Component {
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+{this.props.by}</button>
+        <CounterButton by={2} incrementMethod={this.increment} />
+        <CounterButton by={5} incrementMethod={this.increment} />
+        <CounterButton by={10} incrementMethod={this.increment} />
         <span className="count">
-            {this.state.counter}
+          {this.state.counter}
         </span>
+      </div>
+    );
+  }
+
+    increment(by) {
+      console.log(`increment from parent - ${by}`);
+      this.setState({
+        counter: this.state.counter + by
+      });
+    }
+}
+
+class CounterButton extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0
+    }
+
+    this.increment = this.increment.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.increment}>+{this.props.by}</button>
       </div>
     );
   }
@@ -29,15 +59,17 @@ class Counter extends Component {
     this.setState({
       counter: this.state.counter + this.props.by
     });
+
+    this.props.incrementMethod(this.props.by);
   }
 
 }
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
   by: 1
 }
 
-Counter.propTypes = {
+CounterButton.propTypes = {
   by: PropTypes.number
 }
 
