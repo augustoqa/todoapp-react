fc1d42400769b2d95b06545cd721bc4bd4388567
Cesar Acual <augustoacual@gmail.com>
2020-03-30 18:30:04 -0600

    Using PreviousState in setState

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -17,7 +17,7 @@ class Counter extends Component {
   render() {
     return (
       <div className="counter">
-        <CounterButton by={2} incrementMethod={this.increment} />
+        <CounterButton by={1} incrementMethod={this.increment} />
         <CounterButton by={5} incrementMethod={this.increment} />
         <CounterButton by={10} incrementMethod={this.increment} />
         <span className="count">
@@ -28,9 +28,8 @@ class Counter extends Component {
   }
 
     increment(by) {
-      console.log(`increment from parent - ${by}`);
-      this.setState({
-        counter: this.state.counter + by
+      this.setState((prevState) => {
+          return { counter: prevState.counter + by }
       });
     }
 }
