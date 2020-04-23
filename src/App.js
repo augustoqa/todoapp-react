3d3977034c68f057bf38c84e627b8732704f54c8
Cesar Acual <augustoacual@gmail.com>
2020-04-23 17:21:19 -0600

    Getting started with login component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
-import FirstComponent from './components/learning-examples/FirstComponent';
-import SecondComponent from './components/learning-examples/SecondComponent';
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <TodoApp/>
     </div>
   );
-}
-
-class LearningComponents extends Component {
-    render() {
-        return (
-            <div className="LearningComponents">
-                My Hello World
-                <FirstComponent />
-                <SecondComponent />
-                <ThirdComponent />
-            </div>
-        );
-    }
 }
 
 export default App;
